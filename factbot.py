5d8c5d02efebae3e1beade8b96f6404290b1c207
DanielNormandia <akiraxion@gmail.com>
2017-01-21 19:11:32 -0500

    connect prototype scraper to its thread, add lamba calc file, add css functionality, add facts page

--- factbot.py
+++ factbot.py
@@ -2,7 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import html
 
-page = requests.get('http://www.randomfunfacts.com/')
-tree = (page.content)
-soup = BeautifulSoup(tree, 'html.parser')
-soup.find_all("i")[0].string
+def scrapeFacts():
+    page = requests.get('http://www.randomfunfacts.com/')
+    tree = (page.content)
+    soup = BeautifulSoup(tree, 'html.parser')
+    fact = soup.find_all("i")[0].string
+    print(fact)
+
+
+scrapeFacts()
--- manage.py
+++ manage.py
@@ -6,40 +6,41 @@ import time
 import threading
 from threading import Thread
 from time import sleep
+from factbot import scrapeFacts
 
 keepGoing = True
 
-def job():
-    print("I'm working...")
+# def job():
+#     print("I'm working...")
 
-schedule.every().day.at("14:18").do(job)
-# schedule.every(1).seconds.do(job)
+schedule.every().day.at("12:51").do(scrapeFacts)
+# schedule.every(10).seconds.do(scrapeFacts)
 
 def runScheduler():
     while keepGoing:
         schedule.run_pending()
-        time.sleep(0.1)
+        time.sleep(1)
 
 if __name__ == "__main__":
     thread = Thread(target = runScheduler)
     thread.start()
     #time.sleep(5)
-    #keepGoing = False
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fact_bot.settings")
-    # try:
-    #     from django.core.management import execute_from_command_line
-    # except ImportError:
-    #     # The above import may fail for some other reason. Ensure that the
-    #     # issue is really that Django is missing to avoid masking other
-    #     # exceptions on Python 2.
-    #     try:
-    #         import django
-    #     except ImportError:
-    #         raise ImportError(
-    #             "Couldn't import Django. Are you sure it's installed and "
-    #             "available on your PYTHONPATH environment variable? Did you "
-    #             "forget to activate a virtual environment?"
-    #         )
-    #     raise
-    # execute_from_command_line(sys.argv)
+    # keepGoing = False
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fact_bot.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
 

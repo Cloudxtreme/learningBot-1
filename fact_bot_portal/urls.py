59a30f2723b50cda24d15e00e6ed4e9809f36159
DanielNormandia <akiraxion@gmail.com>
2017-01-21 20:16:38 -0500

    add fact_new route, add fact_edit.html

--- fact_bot_portal/urls.py
+++ fact_bot_portal/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.fact_show, name='fact_show'),
+    url(r'^fact/new/$', views.fact_new, name='fact_new'),
 ]
--- fact_bot_portal/views.py
+++ fact_bot_portal/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .models import Fact
+from .forms import FactForm
+from django.shortcuts import redirect
 # Create your views here.
 def fact_show(request):
     facts = Fact.objects.all()
     return render(request, 'fact_bot_portal/fact_show.html', {'facts': facts})
 
+def fact_new(request):
+    if request.method == "POST":
+        form = FactForm(request.POST)
+        if form.is_valid():
+            fact = form.save(commit=False)
+            fact.save()
+            return redirect('')
+    else:
+        form = FactForm()
+    return render(request, 'fact_bot_portal/fact_edit.html', {'form': form})
